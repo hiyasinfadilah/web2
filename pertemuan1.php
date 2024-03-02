27990a3005c22d5777d0ccf58c759427f3a2f51d
hiyasinfadilah <yasin08.fadilah@gmail.com>
2024-03-02 13:52:05 +0700

    first init github

---
+++ pertemuan1.php
@@ -0,0 +1,25 @@
+<?php 
+print"<pre>";
+print "<center><font size=14 color='magenta'>BIODATA</font></center>";
+echo "<hr size=4 width=60% color='blue'>";
+print "<center><font size=12 color='red'> Nama      : Yasin Fadillah </font><br></center>";
+print "<center><font size=12 color='red'> Email     : yasin08.fadilah@gmail.com </font><br></center>";
+print "<center><font size=12 color='red'> Jurusan   : Teknik Informatika </font><br></center>";
+print "<center><font size=12 color='red'> Hobby     : Healing </font><br></pre></center>";
+echo "<hr size=4 width=60% color='blue'>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
